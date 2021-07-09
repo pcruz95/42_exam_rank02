6cd62fa08fda43cd466ac0f28e995ca74ba09da9
pedro cruz <pedro_cruz_1995@hotmail.com>
2021-07-09 19:37:19 +0100

    100%

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -2,12 +2,11 @@
 
 int get_next_line(char **line)
 {
-	int		i;
-	int		r;
-	char	c;
-	char	*buffer;
+	int i = 0;
+	int r;
+	char c;
+	char *buffer;
 
-	i = 0;
 	if (!(buffer = (char *)malloc(10000)))
 		return (-1);
 	while ((r = read(0, &c, 1)) && c != '\n' && c != '\0')
